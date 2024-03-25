3c4e941cbf3a60434ba018b95768c1dc73446f80
ShubhUniyal <s223531994@deakin.edu.au>
2024-03-26 02:02:48 +1100

    Task3.2p completed

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require("express")
 var app = express()
 
-
+app.use(express.static(__dirname+ '/public'))
+app.use(express.json()); 
+app.use(express.urlencoded( {extended:false}));
 
 
 var port = process.env.port || 3000;
