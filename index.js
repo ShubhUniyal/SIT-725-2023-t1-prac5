cdc252da4b48db6ec11b3aa2087101b94ed63a91
ShubhUniyal <s223531994@deakin.edu.au>
2024-04-09 19:45:55 +1000

    Added the card by using server data

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 var express = require("express")
 var app = express()
 
-app.use(express.static(__dirname+ '/public'))
-app.use(express.json()); 
-app.use(express.urlencoded( {extended:false}));
+app.use(express.static(__dirname + '/public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+const cardList = [
+    { name: 'Student Clubs', desc: 'Great way to meet like-minded individuals, develop leadership skills. ', img: 'https://img.freepik.com/premium-vector/happy-students-with-books-isolated-background_165429-745.jpg' },
+    { name: 'Fitness Classes', desc: 'Attend fitness classes or utilize the college gym facilities', img: 'https://t3.ftcdn.net/jpg/02/35/79/28/360_F_235792895_0SoLUsJ2PVOnjLWRtAqd6gLDuKJaDw93.jpg' },
+    { name: 'Cultural Events', desc: ' Attend cultural events, performances, art exhibitions.', img: 'https://png.pngtree.com/element_our/20200703/ourlarge/pngtree-cartoon-cute-vector-free-female-dance-student-in-dance-training-class-image_2300688.jpg' },
+];
+app.get('/api/cards', (req, res) => {
+    res.json({ statusCode: 200, data: cardList, message: "Success" })
+})
 
 var port = process.env.port || 3000;
-app.listen(port,()=>{
-console.log("App listening to: "+port)
+app.listen(port, () => {
+    console.log("App listening to: " + port)
 })
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -14,40 +14,44 @@ function createCard(name, desc, img) {
     `;
 }
 
-
-const cards = [
-    { name: 'Student Clubs', desc: 'Great way to meet like-minded individuals, develop leadership skills. ', img: 'https://img.freepik.com/premium-vector/happy-students-with-books-isolated-background_165429-745.jpg' },
-    { name: 'Fitness Classes', desc: 'Attend fitness classes or utilize the college gym facilities', img: 'https://t3.ftcdn.net/jpg/02/35/79/28/360_F_235792895_0SoLUsJ2PVOnjLWRtAqd6gLDuKJaDw93.jpg' },
-    { name: 'Cultural Events', desc: ' Attend cultural events, performances, art exhibitions.', img: 'https://png.pngtree.com/element_our/20200703/ourlarge/pngtree-cartoon-cute-vector-free-female-dance-student-in-dance-training-class-image_2300688.jpg' },
-];
 const clickMe = () => {
     alert("Thanks for clicking me. Hope you have a nice day!")
-    }
-    const submitForm = () => {
+}
+const submitForm = () => {
     let formData = {};
     formData.first_name = $('#first_name').val();
     formData.last_name = $('#last_name').val();
     formData.password = $('#password').val();
     formData.email = $('#email').val();
     console.log("Form Data Submitted: ", formData);
-    clickMe();
     $('#modal1').modal('close');
-    }
+
+}
+
+const getcards = () => {
+    $.get('/api/cards', (response) => {
+        if (response.statusCode == 200) {
+            addCards(response.data);
+        }
+    })
+}
+
+const addCards = (cards) => {
+    let cardsContainer = $("#card-container");
+    cards.forEach((card) => {
+        cardsContainer.append(createCard(card.name, card.desc, card.img));
+    });
+}
+
 
 $(document).ready(function () {
     $('.materialboxed').materialbox();
     $('#clickMeButton').click(() => {
         $('#modal1').modal('open');
     });
-    
+    getcards();
     $('.modal').modal();
-    $('#formSubmit').click(()=>{
+    $('#formSubmit').click(() => {
         submitForm();
-        })
-    for (let card of cards) {
-        $('#card-container').append(createCard(card.name, card.desc, card.img));
-    }
+    })
 });
-
-
-
